09be9d4e4948773d266d07eb3082bdcface3698a
Pirathikaran <vpirathikaran18@gmail.com>
2020-04-02 23:46:43 +0530

    Updates

---
+++ precache-manifest.81eb6e41e592a18e529c660ce4f377c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fa5ba1747905ce72cb2acec81257e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fab65e312838b58b597",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ce81686fcd5300c55b6a",
+    "url": "/static/js/2.3b336062.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b336062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fab65e312838b58b597",
+    "url": "/static/js/main.928ecef0.chunk.js"
+  },
+  {
+    "revision": "4d752c1a7bbcc999a468",
+    "url": "/static/js/runtime-main.4aaaec08.js"
+  }
+]);
