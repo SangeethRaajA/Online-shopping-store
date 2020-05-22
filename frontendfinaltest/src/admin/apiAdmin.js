484f28239e2b5f2c58896b031c01c55ffbf8c3fc
Sangeeth Raj <47982022+SangeethRaajA@users.noreply.github.com>
2020-05-22 22:10:54 +0530

    remove order

--- frontendfinaltest/src/admin/apiAdmin.js
+++ frontendfinaltest/src/admin/apiAdmin.js
@@ -75,16 +75,3 @@ export const updateDiscount = (product)=>{
 
 }
 
-export const listOrders = (userId, token) => {
-    return fetch(`${API}/order/list/${userId}`, {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${token}`
-        }
-    })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
-};
